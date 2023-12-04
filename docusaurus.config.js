a460f13f1820f1f3b775721a38e6edb687b23947
Bernardo Freitas <a79295@ualg.pt>
2023-12-04 10:40:11 +0000

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Site Demonstracao',
-  tagline: 'O docusaurus é bacano!',
+  tagline: 'O docusaurus é nada!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
